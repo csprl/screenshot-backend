d11924e32200be77e2a3faaab427030d0637a158
Casper Löfgren <casper.lofgren@hotmail.com>
2016-12-03 00:21:55 +0200

    Add morgan http logger, add a configfile

--- main.js
+++ main.js
@@ -1,17 +1,21 @@
 //  Screenshot Backend
 //  Application entry point
 
+var config = require("./config.json");
 var express = require("express");
+var morgan = require("morgan");
 var app = express();
+
+app.use(morgan("dev"));
 
 app.get("/", function (req, res) {
     res.send("screenshot-backend");
-})
+});
 
 app.post("/upload", function (req, res) {
     res.send("Work in progress");
-})
+});
 
-app.listen(3000, function () {
-    console.log("App listening on port 3000!");
-})
+app.listen(config.port, function () {
+    console.log("App listening on port " + config.port);
+});
