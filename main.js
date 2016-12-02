e2133716e1c3e93adbd74a498c4f24c11ea67b6b
Casper Löfgren <casper.lofgren@hotmail.com>
2016-12-02 23:04:24 +0200

    Basic web code

--- main.js
+++ main.js
@@ -1,2 +1,17 @@
 //  Screenshot Backend
 //  Application entry point
+
+var express = require("express");
+var app = express();
+
+app.get("/", function (req, res) {
+    res.send("screenshot-backend");
+})
+
+app.post("/upload", function (req, res) {
+    res.send("Work in progress");
+})
+
+app.listen(3000, function () {
+    console.log("App listening on port 3000!");
+})
